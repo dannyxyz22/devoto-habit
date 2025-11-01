9369f33e5f402698976138382f09c94f354a424c
Daniel <danielbaggio@gmail.com>
2025-10-31 22:37:24 -0300

    Fix site

--- blog/astro.config.mjs
+++ blog/astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://www.ignisverbi.app',
+	site: 'https://www.ignisverbi.app/blog',
 	base: '/blog',
 	outDir: '../dist/blog',            // 👈 gera os arquivos no dist geral do site
 	integrations: [mdx(), sitemap()],
